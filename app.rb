fa0db3e01176247b7461cabfedc027c5590a82cf
rgrnwd <rgrnwd@thoughtworks.com>
2019-08-04 13:27:53 +1000

    print expected output from app.rb

--- app.rb
+++ app.rb
@@ -1,1 +1,14 @@
+require "./lib/csv_reader.rb"
+require "./lib/calculators/energy_generation_calculator.rb"
+require "./lib/calculators/energy_consumption_calculator.rb"
+
 puts 'Welcome to the Electricity Calculator!'
+puts '======================================'
+
+puts 'Reading ./data/01-solar_generation.csv to calculate energy generation...'
+energy_readings = CSVReader.parse(File.read('./data/01-solar_generation.csv'))
+puts "#{EnergyGenerationCalculator.total_energy_generated(energy_readings)}Wh of electricity was generated"
+
+puts 'Reading ./data/02-consumption.csv to calculate energy generation...'
+energy_readings = CSVReader.parse(File.read('./data/02-consumption.csv'))
+puts "#{EnergyConsumptionCalculator.total_energy_consumed(energy_readings)}Wh of electricity was consumed"
